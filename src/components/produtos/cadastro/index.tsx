de6e336fbeaeaa1da2a9bed2f5f07157da3c42c8
Nilson-Sj <nilsonjr180795@gmail.com>
2023-01-11 11:05:08 -0300

    Created the product form

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -2,8 +2,49 @@ import { Layout } from 'components'
 
 export const CadastroProdutos: React.FC = () => {
   return (
-    <Layout titulo="Cadastro de Produtos">
-      Aqui vai ficar o meu formulário
+    <Layout titulo="Produtos">
+      <div className='field'>
+        <label className='label' htmlFor='inputSku'>SKU: *</label>
+        <div className='control'>
+          <input className='input'
+            id='inputSku'
+            placeholder='Digite o SKU do Produto' />
+        </div>
+      </div>
+      <div className='field'>
+        <label className='label' htmlFor='inputPreco'>Preço: *</label>
+        <div className='control'>
+          <input className='input'
+            id='inputPreco'
+            placeholder='Digite o Preço do Produto' />
+        </div>
+      </div>
+      <div className='field'>
+        <label className='label' htmlFor='inputNome'>Nome: *</label>
+        <div className='control'>
+          <input className='input'
+            id='inputNome'
+            placeholder='Digite o Nome do Produto' />
+        </div>
+      </div>
+      <div className='field'>
+        <label className='label' htmlFor='inputDesc'>Descrição: *</label>
+        <div className='control'>
+          <textarea className='textarea'
+            id='inputDesc'
+            placeholder='Digite a Descrição detalhada do Produto' />
+        </div>
+      </div>
+
+      <div className='field is-grouped'>
+        <div className='control'>
+          <button className='button'>Salvar</button>
+        </div>
+        <div className='control'>
+          <button className='button'>Voltar</button>
+        </div>
+      </div>
+
     </Layout>
   )
 }
